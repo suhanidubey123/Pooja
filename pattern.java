f1c3034ab7ec82a88c4b8a3f27dfa451cff5df05
Suhani <144649408+suhanidubey123@users.noreply.github.com>
2024-11-05 14:37:10 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,21 @@
+class pattern {
+    public static void main(String arg[])
+    {
+        System.out.println("Here your pattern:");
+int i,j,row=5;
+        
+            for(i=1;i<=row;i++)
+        {
+            for(j=row-i;j>0;j--)
+            {
+                System.out.print("  ");}
+            for(j=1;j<=i;j++)
+            {System.out.print("* ");}
+            System.out.print("\n");}
+             
+    }
+    
+
+    }      
+
+    
